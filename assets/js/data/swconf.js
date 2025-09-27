015ea8629f8b10421b016781ac06fb1f39b95b20
jeffreybergier <jeffreybergier@users.noreply.github.com>
2025-09-27 23:34:37 +0000

    deploy: 1072c04bfc2dc298930faf5cb3b65cb244b6e4c5

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1759016075',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/apps/',
+      
+        '/design/',
+      
+        '/retro-tech/',
+      
+        '/all-articles/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+

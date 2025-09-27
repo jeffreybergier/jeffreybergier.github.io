5c7f154d67be4e25d286b89c81af40f639cb246f
jeffreybergier <jeffreybergier@users.noreply.github.com>
2025-09-27 23:46:17 +0000

    deploy: db668c5ad764a0c1dbd265c7ea5d2e9b7cba2fe5

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759016075',resources: [
+    cacheName: 'chirpy-1759016776',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
